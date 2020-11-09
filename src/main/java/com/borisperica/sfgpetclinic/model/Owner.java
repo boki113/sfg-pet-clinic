f2ae8f67c95ddf8fc2b26057ba88306aa34b2103
Boris Perica <borisperper@gmail.com>
2020-11-09 14:22:24 +0100

    implemented basic POJOs

---
+++ src/main/java/com/borisperica/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.borisperica.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/borisperica/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.borisperica.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
