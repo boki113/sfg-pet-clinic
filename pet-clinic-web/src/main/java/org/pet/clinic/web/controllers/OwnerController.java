725943b3294e569af9a5622a083fb4270edc8d96
Boris Perica <borisperper@gmail.com>
2020-11-18 15:10:57 +0100

    added owners html and controller

---
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/controllers/OwnerController.java
@@ -0,0 +1,19 @@
+package org.pet.clinic.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author perica
+ *
+ */
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+	
+	@RequestMapping({"","/", "/index", "/index.html"})
+	public String listOwners() {
+		return "owners/index";
+	}
+}
