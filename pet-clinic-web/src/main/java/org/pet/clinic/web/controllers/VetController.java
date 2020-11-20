df8a6928a1682a3c59931058c8fd3b20041a545e
Boris Perica <borisperper@gmail.com>
2020-11-20 13:32:55 +0100

    listed all vets on vet index page. close #13

--- pet-clinic-web/src/main/java/org/pet/clinic/web/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/controllers/VetController.java
@@ -1,6 +1,8 @@
 package org.pet.clinic.web.controllers;
 
+import org.pet.clinic.data.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,10 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
+@RequestMapping("vets")
 public class VetController {
 	
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
+	@RequestMapping({"","/", "/index", "/index.html"})
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
