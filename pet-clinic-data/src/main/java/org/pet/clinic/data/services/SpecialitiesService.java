a3760b8f8ae58a4be3cdd45828fbdc5bbb86258c
Boris Perica <borisperper@gmail.com>
2020-11-27 13:04:22 +0100

    Added specialities map impl

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package org.pet.clinic.data.services;
+
+import org.pet.clinic.data.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long>{
+	
+}
---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/services/map/SpecialityMapService.java
@@ -0,0 +1,38 @@
+package org.pet.clinic.data.services.map;
+
+import java.util.Set;
+
+import org.pet.clinic.data.model.Speciality;
+import org.pet.clinic.data.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality entity) {
+		return super.save(entity);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality entity) {
+		super.delete(entity);
+	}
+
+	
+}
